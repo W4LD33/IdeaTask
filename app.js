5ecc2def3df9e2f5c3810a89f4393a9d5da63401
W4LD33 <girstautas.valdemaras@gmail.com>
2023-04-03 20:53:01 +0300

    first commit

--- app.js
+++ app.js
@@ -2,11 +2,12 @@ const express = require('express')
 const app = express()
 
 app.get('/removeLetter/:word/:letter', (req, res) => {
-  const word = req.params.word
-  const letter = req.params.letter
-  const regex = new RegExp(letter, 'gi')
-  const newWord = word.replace(regex, '')
-  res.send(newWord)
+    res.header("Access-Control-Allow-Origin", "*");
+    const word = req.params.word
+    const letter = req.params.letter
+    const regex = new RegExp(letter, 'gi')
+    const newWord = word.replace(regex, '')
+    res.send(newWord)
 })
 
 const port = process.env.PORT || 3000
